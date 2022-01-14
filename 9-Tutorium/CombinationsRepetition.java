d37fbf5afd9a75919f74db288843e74f17a79462
EagleoutIce <florian.sihler@uni-ulm.de>
2022-01-14 01:25:13 +0100

    tut9 base

--- 9-Tutorium/CombinationsRepetition.java
+++ 9-Tutorium/CombinationsRepetition.java
@@ -1,4 +1,4 @@
-public class Combinations {
+public class CombinationsRepetition {
     public static void printArray(int[] combination, int i) {
         if (i < combination.length) {
             System.out.print(combination[i] + " ");
@@ -8,26 +8,26 @@ public class Combinations {
         }
     }
 
-    private static void helper(int[] array, int n, int[] combination, int start, int index) {
+   private static void helper(int[] array, int n, int[] combination, int index) {
         if (index == n) {
             printArray(combination, 0);
         } else {
-            for (int i = start; i < array.length && array.length - i >= n - index; i++) {
+            for (int i = 0; i < array.length; i ++) {
                 combination[index] = array[i];
-                helper(array, n, combination, i + 1, index + 1);
+                helper(array, n, combination, index + 1);
             }
-        }
-    }
+       }
+   }
 
     public static void printCombinations(int[] array, int n) {
         int[] combination = new int[n];
-        helper(array, n, combination, 0, 0);
+        helper(array, n, combination, 0);
     }
 
     public static void main(String[] args) {
         int[] array = { 1, 2, 3, 4, 5 };
-        int n = 2;
-        System.out.print("Die Kombinationen ohne Wiederholung aus [");
+        int n = 3;
+        System.out.print("Die Kombinationen mit Wiederholung aus [");
         for (int i = 0; i < array.length - 1; i++)
             System.out.print(array[i] + ",");
         System.out.println(array[array.length - 1] + "] mit n=" + n + " sind:");
