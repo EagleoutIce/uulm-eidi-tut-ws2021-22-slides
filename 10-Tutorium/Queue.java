75f4ae14155603906cad1f5c9d4387473fc8ca23
Florian Sihler <florian.sihler@uni-ulm.de>
2022-01-22 19:09:45 +0100

    moving pingu banner

---
+++ 10-Tutorium/Queue.java
@@ -0,0 +1,106 @@
+public class Queue {
+   class Element {
+       private final int value;
+       private Element next;
+
+       public Element(int value) {
+           this.value = value;
+           this.next = null;
+       }
+
+       public void setNextElement(Element nextElement) {
+           this.next = nextElement;
+       }
+
+       public Element getNextElement() {
+           return this.next;
+       }
+
+       public int getValue() {
+           return this.value;
+       }
+   }
+
+   private Element first;
+   private Element last;
+   private int length;
+
+   public Queue() {
+       this.first = null;
+       this.last = null;
+       this.length = 0;
+   }
+
+   public void enqueue(int value) {
+       Element newElement = new Element(value);
+
+       if(length == 0) {
+           this.first = newElement;
+           this.last = newElement;
+       } else {
+           this.last.setNextElement(newElement);
+           this.last = newElement;
+       }
+       length++;
+   }
+
+   public boolean dequeue() {
+       if(length > 0) {
+           this.first = this.first.getNextElement();
+           length--;
+           return true;
+       }
+       return false;
+   }
+
+   public int getLength() {
+       return this.length;
+   }
+
+   public Integer getFirst() {
+       if(length > 0)
+           return first.getValue();
+       return null;
+   }
+
+   public Integer getLast() {
+       if(length > 0)
+           return last.getValue();
+       return null;
+   }
+
+   public String toString() {
+       String s = "[";
+       if(length > 0) {
+           Element next = this.first;
+           while (next.getNextElement() != null) {
+               s += next.getValue() + ", ";
+               next = next.getNextElement();
+           }
+           s += next.getValue();
+       }
+       s += "]";
+       return s;
+   }
+
+   public static void main(String[] args) {
+       Queue queue = new Queue();
+       queue.enqueue(3);
+       queue.enqueue(1);
+       queue.enqueue(7);
+       queue.enqueue(5);
+       queue.enqueue(9);
+
+       System.out.println("Die Queue: " + queue.toString() + " hat eine Länge von " + queue.getLength());
+       if(queue.dequeue())
+           System.out.println("Das erste Element wurde entfernt");
+       else
+           System.out.println("Entfernen des ersten Elements war nicht möglich");
+
+       if(queue.getFirst() != null)
+           System.out.println("Das neue erste Element ist: " + queue.getFirst());
+
+       if(queue.getLast() != null)
+           System.out.println("Das letzte Element ist immer noch: " + queue.getLast());
+   }
+}
