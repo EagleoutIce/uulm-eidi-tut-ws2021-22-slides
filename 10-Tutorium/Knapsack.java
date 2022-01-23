9c30bc8fda58e5303e15d13e29d1554ae193c527
Florian Sihler <florian.sihler@uni-ulm.de>
2022-01-23 22:47:02 +0100

    run through t10

--- 10-Tutorium/Knapsack.java
+++ 10-Tutorium/Knapsack.java
@@ -1,36 +1,54 @@
 public class Knapsack {
-    public static List knapsack(int capacity, int[] values, int[] weights) {
-        if (capacity < 0)
-            throw new IllegalArgumentException("Negative capacity");
-        if(values.length != weights.length)
-            throw new IllegalArgumentException("Array lengths differ");
-        return knapsack(capacity, 0, values, weights);
+    private static int helper(int W, int i, int[] weights, int[] values) {
+        if (i == weights.length || W == 0)
+            return 0;
+
+        if (weights[i] > W)
+            return helper(W, i + 1, weights, values);
+        else
+            return Math.max(helper(W, i + 1 , weights, values),
+                            helper(W - weights[i], i + 1,  weights, values) + values[i]);
     }
 
-    private static List knapsack(int capacity, int index, int[] values, int[] weights) {
-        if(index >= values.length)
-            return new List();
-        // W < w_i
-        if(capacity < weights[index])
-            return knapsack(capacity, index + 1, values, weights);
-        List withItem = knapsack(capacity - weights[index], index + 1, values, weights).push(index);
-        List withoutItem = knapsack(capacity, index + 1, values, weights);
+    public static int knapsack(int W, int[] weights, int[] values) {
+        if(weights.length != values.length)
+            return -1;
 
-        int valueWithItem = getValueOfList(withItem, values);
-        int valueWithoutItem = getValueOfList(withoutItem, values);
-
-        return valueWithItem > valueWithoutItem ? withItem : withoutItem;
+        return helper(W, 0, weights, values);
     }
 
-    private static int getValueOfList(List list, int[] values) {
-        int sum = 0;
-        for(int i = 0; i < list.length; i++) {
-            sum += values[list.getByIndex(i)];
+    public static void printArray(int[] array) {
+        System.out.print("[");
+        for(int i=0; i < array.length-1; i++)
+            System.out.print(array[i] + ", ");
+        System.out.println(array[array.length-1] + "]");
+    }
+
+    public static void main(String args[]) {
+        // Generate values for a random number of objects
+        int length = (int) (Math.random() * 6 + 5);
+        int[] values = new int[length];
+        int[] weights = new int[length];
+
+        int sumOfWeights = 0;
+        // Generate random positive values and weights for the objects
+        for(int i = 0; i < length; i++) {
+            values[i] = (int) (Math.random() * 100 + 1);
+            weights[i] = (int) (Math.random() * 100 + 1);
+            sumOfWeights += weights[i];
         }
-        return sum;
-    }
 
-    public static void main(String[] args) {
-        System.out.println(knapsack(6, new int[]{7, 3, 9, 12, 1}, new int[]{5, 4, 3, 2, 1}));
+        System.out.print("Werte: ");
+        printArray(values);
+
+        System.out.print("Gewichte: ");
+        printArray(weights);
+
+        // Compute a reasonable target weight
+        int W = 2 * sumOfWeights / length;
+        System.out.println("Kapazität: " + W + "\n");
+
+        // Execute the actual knapsack algorithm
+        System.out.println("Maximaler Wert: " + knapsack(W, weights, values));
     }
 }
