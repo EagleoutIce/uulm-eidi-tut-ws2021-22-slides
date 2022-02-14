564ef5a5fbb26d23e8d6b82daa1188b8d015659b
Florian Sihler <florian.sihler@uni-ulm.de>
2022-02-14 01:00:17 +0100

    another java file cleanup

--- 9-Tutorium/Node.java
+++
@@ -1,9 +0,0 @@
-public class Node {
-    public final int value;
-    public final Node tail;
-
-    public Node(int value, Node tail) {
-      this.value = value;
-      this.tail = tail;
-    }
-  }
