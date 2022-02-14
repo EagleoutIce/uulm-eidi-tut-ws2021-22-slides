30cfde083fd8b4d769c31894b56717344a67cd9f
Florian Sihler <florian.sihler@uni-ulm.de>
2022-02-14 14:40:03 +0100

    con -> von und PolygonTest adapt

--- 13-Tutorium/PolygonTest.java
+++ 13-Tutorium/PolygonTest.java
@@ -1,8 +1,8 @@
 public class PolygonTest {
    public static void main(String[] args) {
-      RegularPolygon triangle = new EquilateralTriangle(3); // Aufruf 1
+      RegularPolygon polygon = new EquilateralTriangle(3); // Aufruf 1
 
-      EquilateralTriangle newTriangle = triangle; // Aufruf 2
+      EquilateralTriangle newTriangle = (EquilateralTriangle) polygon; // Aufruf 2
 
       Square square = new RegularPolygon(4, 4); // Aufruf 3
    }
